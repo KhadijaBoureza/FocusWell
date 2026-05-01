12f0daa2b43e35b1106bfeaa10ef3c4efadb3e81
KhadijaBoureza <165843299+KhadijaBoureza@users.noreply.github.com>
2026-05-01 01:31:55 +0000

    Move database connection logic to server entry point

--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
 const app = require("./app");
+const connectDB = require("./db");
 
 const PORT = 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
 });
