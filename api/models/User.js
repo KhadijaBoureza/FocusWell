e878c55ce509b7c315794ca6eacb51596a05c933
KhadijaBoureza <165843299+KhadijaBoureza@users.noreply.github.com>
2026-05-04 23:18:58 +0000

    Add User model for authentication

---
+++ api/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    displayName: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 60,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
